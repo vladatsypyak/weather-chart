9793568c410b844fa4643d64a6d34b791a3813dc
Vladyslava <vladatsyupyak@gmail.com>
2025-05-31 23:25:17 +0200

    feat: display chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import CitySearch from "./components/CitySearch/CitySearch";
+import Chart from "./components/Chart/Chart";
 
 function App() {
     return (
         <div className="App">
             <CitySearch/>
+            <Chart/>
         </div>
     );
 }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,14 +1,26 @@
-import {useState} from "react";
+import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
+import {getWeather} from "../../redux/slice";
+import {Bar, BarChart, Tooltip, XAxis, YAxis} from "recharts";
 
 
 function Chart() {
     const city = useSelector(store => store.weatherSlice.city)
-
+    const forecast = useSelector(store => store.weatherSlice.weather)
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(getWeather(city))
+    }, [dispatch, city])
 
 
     return (
         <div className="chart_wrap">
+            {city &&  <BarChart width={730} height={250} data={forecast} margin={{top: 20, right: 20, bottom: 20, left: 20}} >
+                <XAxis dataKey="day" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="data" fill="#8884d8" />
+            </BarChart>}
 
         </div>
     );
