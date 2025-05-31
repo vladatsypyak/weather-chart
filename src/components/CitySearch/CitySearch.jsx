d8501a48cd7ad89c9292cfd309a6fba699da521e
Vladyslava <vladatsyupyak@gmail.com>
2025-05-31 11:34:07 +0200

    feat: create citySearch component

--- src/components/CitySearch/CitySearch.jsx
+++ src/components/CitySearch/CitySearch.jsx
@@ -1,7 +1,25 @@
+import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {getFirstCity} from "../../redux/slice";
 
 function CitySearch() {
+    const city = useSelector(store => store.weatherSlice.city)
+    const [cityInput, setCityInput] = useState("")
+    const dispatch = useDispatch()
+
+    const onSearchClick = async () => {
+        if (!cityInput) return
+        await dispatch(getFirstCity(cityInput))
+        if (!city) {
+          window.alert("Sorry, no city was found")
+        }
+    }
+
+
     return (
         <div className="CitySearch">
+            <input type="text" onInput={(e) => setCityInput(e.target.value)}/>
+            <button onClick={onSearchClick}> search</button>
         </div>
     );
 }
