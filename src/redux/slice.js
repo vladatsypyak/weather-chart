f05e6d2b57d1f127884de68bc3c772c322b02072
Vladyslava <vladatsyupyak@gmail.com>
2025-05-31 23:19:02 +0200

    feat: fetch weather and handle response

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -1,16 +1,24 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
 import axios from "axios";
+import moment from "moment";
+
 
 const fetchCities = async (city) => await axios
     .get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.REACT_APP_API_KEY}`)
     .then(resp => resp.data)
     .catch(error => console.log(error))
 
+const fetchWeather = async (city) => {
+    return await axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
+        .then(resp => resp.data)
+        .catch(error => console.log(error))
+}
+
 export const getFirstCity = createAsyncThunk('city/getCity',
     async (city, thunkAPI) => {
         try {
-           const data =  await fetchCities(city)
-            if(Array.isArray(data) && data.length > 0){
+            const data = await fetchCities(city)
+            if (Array.isArray(data) && data.length > 0) {
                 return {
                     name: data[0].name,
                     lon: data[0].lon,
@@ -29,8 +37,39 @@ export const getFirstCity = createAsyncThunk('city/getCity',
         }
     })
 
+const getForecastForEachDay = (dates, details) => {
+    return dates.map((day) => {
+            const allTemperatures = details.filter(el => el.day === day).map(el => el.temp).flat()
+        return {day, data: [Math.max(...allTemperatures), Math.min(...allTemperatures)]}
+        })
+}
+
+export const getWeather = createAsyncThunk('weather/getWeather',
+    async (city, thunkAPI) => {
+        try {
+            if (!city) return null
+            const data = await fetchWeather(city)
+            const details = data.list.map(el => {
+                return {temp: [el.main.temp_max, el.main.temp_min], day: (moment(new Date(el.dt * 1000)).format("dddd, DD.MM"))}
+            })
+            const dates = [...new Set(details.map(el => el.day))]
+            return getForecastForEachDay(dates, details)
+
+        } catch
+            (error) {
+            console.error(error)
+            if (error instanceof Error) {
+                return thunkAPI.rejectWithValue(error.message);
+            } else {
+                return thunkAPI.rejectWithValue('Error when fetching weather');
+            }
+        }
+    }
+)
+
 const initialState = {
     city: null,
+    weather: null,
     loading: false,
     error: null,
 }
@@ -52,6 +91,19 @@ export const weatherSlice = createSlice({
                 state.error = null;
             })
             .addCase(getFirstCity.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload ?? 'Failed to fetch city';
+            })
+            .addCase(getWeather.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(getWeather.fulfilled, (state, action) => {
+                state.weather = action.payload
+                state.loading = false;
+                state.error = null;
+            })
+            .addCase(getWeather.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.payload ?? 'Failed to fetch city';
             })
