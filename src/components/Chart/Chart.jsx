8eb1df3600d4c38b440007de4e659e5c8a2703df
Vladyslava <vladatsyupyak@gmail.com>
2025-06-01 14:20:17 +0200

    feat: move not chart related code to app component

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,21 +1,13 @@
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {getWeather} from "../../redux/slice";
+import {useSelector} from "react-redux";
 import {Bar, BarChart, Tooltip, XAxis, YAxis} from "recharts";
+import s from "./chart.module.scss"
 
 
 function Chart() {
-    const city = useSelector(store => store.weatherSlice.city)
     const forecast = useSelector(store => store.weatherSlice.weather)
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(getWeather(city))
-    }, [dispatch, city])
-
-
     return (
-        <div className="chart_wrap">
-            {city ? <BarChart layout={'vertical'} width={900} height={400} data={forecast}
+        <div className={s.chart_wrap}>
+          <BarChart layout={'vertical'} width={900} height={400} data={forecast}
                               margin={{top: 20, right: 20, bottom: 20, left: 20}}>
                     <XAxis type="number" domain={['auto', 'auto']} unit={"°C"}/>
                     <YAxis type="category" dataKey="day"/>
@@ -27,9 +19,7 @@ function Chart() {
                     }
                     }/>
                     <Bar dataKey="temp" fill="#8884d8" unit={"°C"}/>
-                </BarChart> :
-                <p>Please enter a city to see the forecast</p>}
-
+                </BarChart>
         </div>
     );
 }
