05b10060e2f85c898e8647d0d97864b096868eda
Vladyslava <vladatsyupyak@gmail.com>
2025-06-01 11:16:07 +0200

    fix: remove "temp" in tooltip

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -8,19 +8,26 @@ function Chart() {
     const city = useSelector(store => store.weatherSlice.city)
     const forecast = useSelector(store => store.weatherSlice.weather)
     const dispatch = useDispatch()
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getWeather(city))
     }, [dispatch, city])
 
 
     return (
         <div className="chart_wrap">
-            {city ? <BarChart layout={'vertical'} width={900} height={400} data={forecast} margin={{top: 20, right: 20, bottom: 20, left: 20}} >
-                <XAxis type="number" domain={['auto', 'auto']} unit={"°C"} />
-                <YAxis type="category"   dataKey="day" />
-                <Tooltip />
-                <Bar dataKey="temp" fill="#8884d8" unit={"°C"}/>
-            </BarChart> :
+            {city ? <BarChart layout={'vertical'} width={900} height={400} data={forecast}
+                              margin={{top: 20, right: 20, bottom: 20, left: 20}}>
+                    <XAxis type="number" domain={['auto', 'auto']} unit={"°C"}/>
+                    <YAxis type="category" dataKey="day"/>
+                    <Tooltip formatter={(value) => {
+                        if (Array.isArray(value)) {
+                            return [`${value[0]} - ${value[1]}`]
+                        }
+                        return value
+                    }
+                    }/>
+                    <Bar dataKey="temp" fill="#8884d8" unit={"°C"}/>
+                </BarChart> :
                 <p>Please enter a city to see the forecast</p>}
 
         </div>
