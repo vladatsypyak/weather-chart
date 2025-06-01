28b56072c373e10b8eb29fb299f36a2837eba394
Vladyslava <vladatsyupyak@gmail.com>
2025-06-01 14:16:36 +0200

    feat: display weather icons when no city

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import {useEffect} from "react";
 import CitySearch from "./components/CitySearch/CitySearch";
 import Chart from "./components/Chart/Chart";
+import WeatherPics from "./components/WeatherPics/WeatherPics";
+import {useDispatch, useSelector} from "react-redux";
+import {getWeather} from "./redux/slice";
 
 function App() {
+    const city = useSelector(store => store.weatherSlice.city)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getWeather(city))
+    }, [dispatch, city])
+
     return (
         <div className="App">
             <CitySearch/>
-            <Chart/>
+            {city ? <Chart/> : <WeatherPics/>}
         </div>
     );
 }
---
+++ src/components/WeatherPics/WeatherPics.jsx
@@ -0,0 +1,24 @@
+import s from "./weatherPics.module.scss"
+import sun from "../../assets/sun.png"
+import cloudy from "../../assets/cloudy.png"
+import temperature from "../../assets/temperature.png"
+
+
+function WeatherPics() {
+
+
+
+    return <div className={s.icons}>
+                    <div className={s.icon}>
+                        <img src={sun} alt=""/>
+                    </div>
+                    <div className={s.icon}>
+                        <img src={cloudy} alt=""/>
+                    </div>
+                    <div className={s.icon}>
+                        <img src={temperature} alt=""/>
+                    </div>
+                </div>
+}
+
+export default WeatherPics;
